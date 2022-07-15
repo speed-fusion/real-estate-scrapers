91293d75638224fc7c1606e299bfb1ef17d8c046
speed-fusion <106366121+speed-fusion@users.noreply.github.com>
2022-07-15 16:52:16 +0530

    done

--- realtor/runner.py
+++ realtor/runner.py
@@ -2,7 +2,7 @@ import os
 
 while True:
     try:
-        os.system("scrapy crawl estimates")
+        os.system("scrapy crawl property_data")
     except Exception as e:
         print(f'error : {str(e)}')
         
